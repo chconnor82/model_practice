41498dca95190ee787ed972c241cdd972f8a6d43
Charlie Connor <chconnor@gmail.com>
2014-05-12 02:04:02 -0500

    Added a v2 into Model_Practicev2
    I could get everything working expect for question five

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,27 +5,35 @@ class QuestionsController < ApplicationController
     # Your Ruby goes here.
     @the_actor = Actor.second.id
     @movies_for_the_second_actor = Movie.where(:the_actor.movies.count => @the_actor)
-    @movies_for_the_second_actor.order("year DESC").first.title
+    @most_recent_movie_for_second_actor = Actor.second.movies.order("year DESC").first
   end
 
   def question_2
     # Who directed the longest movie on the list?
 
     the_longest_movie_director_id = Movie.order("duration ASC").last.director_id
-    # @director_of_longest_movie = ???
     Director.find(the_longest_movie_director_id).name
+    @director_of_longest_movie.name = Movie.order("duration DESC").first.director
   end
 
   def question_3
     # Which director has the most movies on the list?
 
-    movie_counts = {}
+    # movie_counts = {}
+    the_leader = Director.new
     Director.all.each do |the_director|
-    the_director.movies.count
+    # movie_counts[the_director.movies] = the_director.movies.count
+    if the_director.movies.count > top_director.movies.count
+    the_leader = the_director
+  end
+end
+  # @most_number_of_movies_by_a_single_director = movie_counts.sort
+  @director_with_the_most_movies = the_leader
+
   end
 
-  @most_number_of_movies_by_a_single_director = movie_counts.sort
-  @director_with_the_most_movies = (movie_counts.sort).name
+    @director_with_the_most_movies = top_director.name
+
   end
 
   def question_4
@@ -59,5 +67,33 @@ class QuestionsController < ApplicationController
     # @actor = ???
     # @director = ???
     # @movies_together = ???
+    most_movies_together = 0
+     @actor = nil
+     @director = nil
+
+     Actor.all.each do |actor|
+       this_actors_most_movies_with_one_director = 0
+       this_actors_favorite_director = nil
+
+       actor.movies.each do |movie|
+         number_of_movies_with_same_director_as_this_one = actor.movies.where(:director_id => movie.director_id).count
+
+         if this_actors_most_movies_with_one_director < number_of_movies_with_same_director_as_this_one
+           this_actors_most_movies_with_one_director = number_of_movies_with_same_director_as_this_one
+           this_actors_favorite_director = movie.director
+         end
+       end
+
+       if most_movies_together < this_actors_most_movies_with_one_director
+         most_movies_together = this_actors_most_movies_with_one_director
+         @director = this_actors_favorite_director
+         @actor = actor
+       end
+     end
+
+     @movies_together = @actor.movies.where(:director_id => @director.id)
+    end
+  end
+
   end
  end
